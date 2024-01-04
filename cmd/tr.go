04ba2caf48869f1b1510f2b76009f252c14eacd2
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-01-04 17:43:42 +0530

    1. fix: handle unicode point transformations as well

--- cmd/tr.go
+++ cmd/tr.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"regexp"
 	"strings"
+	"unicode/utf8"
 
 	"golang.org/x/exp/slices"
 )
@@ -14,13 +15,13 @@ func isValidRangePattern(r string) (bool, error) {
 	return regexp.MatchString("^.-^|.", r)
 }
 
-func createRangeSubstitutions(r1 []byte, r2 []byte) map[byte]byte {
-	substitutions := make(map[byte]byte)
+func createRangeSubstitutions(r1 []rune, r2 []rune) map[rune]rune {
+	substitutions := make(map[rune]rune)
 	idx := 0
 	n1, n2 := len(r1), len(r2)
 
 	for idx = 0; idx < n2 && idx < n1; idx++ {
-		substitutions[r1[idx]] = r2[idx] /* Assuming only ASCII characters */
+		substitutions[r1[idx]] = r2[idx]
 	}
 
 	/* If len(r1) > len(r2), last character of r2 is duplicated */
@@ -32,12 +33,10 @@ func createRangeSubstitutions(r1 []byte, r2 []byte) map[byte]byte {
 }
 
 /* Assuming a valid pattern matching regexp.MatchString("^.-^|.", r) */
-func createRangeFromPattern(p string) []byte {
-	r := []byte{}
-	start, end := int(p[0]), int(p[0]) /* Get ASCII code */
-	if len(p) > 2 {
-		end = int(p[2])
-	}
+func createRangeFromPattern(p string) []rune {
+	runes := []rune{}
+	start, _ := utf8.DecodeRuneInString(p)
+	end, _ := utf8.DecodeLastRuneInString(p)
 
 	isReverse := end < start
 	if isReverse {
@@ -46,30 +45,31 @@ func createRangeFromPattern(p string) []byte {
 		end = temp
 	}
 
-	for b := start; b <= end; b++ {
-		r = append(r, byte(b))
+	runes = append(runes, start)
+	for i := start; i <= end; i++ {
+		runes = append(runes, i)
 	}
 
 	if isReverse {
-		slices.Reverse(r)
+		slices.Reverse(runes)
 	}
 
-	return r
+	return runes
 }
 
-func substitute(s string, m map[byte]byte) string {
+func substitute(s string, m map[rune]rune) string {
 	var res strings.Builder
 
 	for i := 0; i < len(s); i++ {
-		b := s[i]
+		b := rune(s[i])
 		replacement, inMap := m[b]
 
 		if !inMap {
-			res.WriteByte(b)
+			res.WriteRune(b)
 			continue
 		}
 
-		res.WriteByte(replacement)
+		res.WriteRune(replacement)
 	}
 
 	return res.String()
