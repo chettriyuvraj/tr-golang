46bc00706c2e48d884bb4e10cdf931db918b0221
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-01-05 14:12:02 +0530

    1. feat: restructure tr.go to match pattern type, conv to range and return range 2. feat: add support for class specifiers

--- cmd/tr.go
+++ cmd/tr.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 	"unicode/utf8"
@@ -10,9 +11,33 @@ import (
 
 /**** Helpers ****/
 const HELPSTRING = "usage: tr string1 string2\n"
+const (
+	RANGE  = "RANGE"
+	CLASS  = "CLASS"
+	DIRECT = "DIRECT"
+)
 
-func isValidRangePattern(r string) (bool, error) {
-	return regexp.MatchString("^.-^|.", r)
+func findPatternType(p string) (string, error) {
+	match, err := regexp.MatchString("^.-.$", p)
+	if err != nil {
+		return "", err
+	}
+	if match {
+		fmt.Println("RANGE")
+		return RANGE, nil
+	}
+
+	fmt.Println(p)
+	match, err = regexp.MatchString("^\\[:.*:\\]$", p)
+	if err != nil {
+		return "", err
+	}
+	if match {
+		fmt.Println("CLASS")
+		return CLASS, nil
+	}
+
+	return DIRECT, nil
 }
 
 func createRangeSubstitutions(r1 []rune, r2 []rune) map[rune]rune {
@@ -32,8 +57,28 @@ func createRangeSubstitutions(r1 []rune, r2 []rune) map[rune]rune {
 	return substitutions
 }
 
-/* Assuming a valid pattern matching regexp.MatchString("^.-^|.", r) */
-func createRangeFromPattern(p string) []rune {
+func createRangeFromPattern(p string) ([]rune, error) {
+	var runes []rune
+
+	t, err := findPatternType(p)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t {
+	case RANGE:
+		runes = rangeAsRunes(p)
+	case CLASS:
+		runes = classAsRunes(p)
+	case DIRECT:
+		runes = strAsRunes(p)
+	}
+
+	return runes, nil
+}
+
+/* Convert a pattern of type 'RANGE' into []rune containing each rune of the range */
+func rangeAsRunes(p string) []rune {
 	runes := []rune{}
 	start, _ := utf8.DecodeRuneInString(p)
 	end, _ := utf8.DecodeLastRuneInString(p)
@@ -45,7 +90,6 @@ func createRangeFromPattern(p string) []rune {
 		end = temp
 	}
 
-	runes = append(runes, start)
 	for i := start; i <= end; i++ {
 		runes = append(runes, i)
 	}
@@ -55,6 +99,31 @@ func createRangeFromPattern(p string) []rune {
 	}
 
 	return runes
+}
+
+func strAsRunes(s string) []rune {
+	runes := []rune{}
+	idx := 0
+	for r, size := utf8.DecodeRuneInString(s); idx < len(s); r, size = utf8.DecodeRuneInString(s[idx:]) {
+		runes = append(runes, r)
+		idx += size
+	}
+	return runes
+}
+
+func classAsRunes(class string) []rune {
+	var res []rune
+
+	switch class {
+	case "[:alpha:]":
+		res = []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+	case "[:lower:]":
+		res = []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+	case "[:upper:]":
+		res = []rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+	}
+
+	return res
 }
 
 func substitute(s string, m map[rune]rune) string {
